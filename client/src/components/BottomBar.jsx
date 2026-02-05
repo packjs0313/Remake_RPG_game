165981210d29f93f118deec11d7770be1ac97018
MO-NO-LOG <9338088a@gmail.com>
2026-02-05 15:59:52 +0900

    반응형 하단메뉴 스와이프 기능 추가가

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -1,16 +1,24 @@
-export default function BottomBar() {
+﻿export default function BottomBar() {
   return (
     <div className="bottom-bar">
       <div className="gold">소지금: 100</div>
-      <div className="action-buttons">
-        <button>스탯</button>
-        <button>스킬</button>
-      </div>
-      <div className="skill-list">
-        <div className="skill-slot">스킬없음 (-MP)</div>
-        <div className="skill-slot">스킬없음 (-MP)</div>
-        <div className="skill-slot">스킬없음 (-MP)</div>
-        <div className="skill-slot">스킬없음 (-MP)</div>
+      <div className="mobile-slider">
+        <div className="action-buttons">
+          <button>
+            <img src="./skill.png" alt="스킬" />
+            <span>스킬</span>
+          </button>
+          <button>
+            <img src="./stats.png" alt="스탯" />
+            <span>스탯</span>
+          </button>
+        </div>
+        <div className="skill-list">
+          <div className="skill-slot">스킬없음 (-MP)</div>
+          <div className="skill-slot">스킬없음 (-MP)</div>
+          <div className="skill-slot">스킬없음 (-MP)</div>
+          <div className="skill-slot">스킬없음 (-MP)</div>
+        </div>
       </div>
     </div>
   );
