b8f437bb98cbe0ed6ab9808244c2860be4511583
MO-NO-LOG <9338088a@gmail.com>
2026-02-06 12:36:02 +0900

    Combat-Log Ui만듬

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -20,6 +20,9 @@
           <div className="skill-slot">스킬없음 (-MP)</div>
         </div>
       </div>
+        <div className="Combat-Log">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, rerum veniam quod voluptatum deleniti laudantium quis ipsa aperiam non velit quasi esse aut error tempora odio quae nulla iusto? Vero tenetur eveniet maiores dolorum. Sint adipisci facilis quia, in illo consequatur aliquid culpa perspiciatis, fugit neque ab accusamus ratione itaque voluptates recusandae nemo amet, placeat est vitae rem qui? Assumenda ex veritatis iure nemo quis accusamus vero voluptate recusandae explicabo, magnam praesentium accusantium magni eveniet voluptas nulla, harum vitae amet! Beatae obcaecati repudiandae magnam ipsa autem at dignissimos fugit in nulla saepe. Exercitationem suscipit perspiciatis deserunt, cupiditate non accusantium eligendi.</p>
+        </div>
     </div>
   );
 }
